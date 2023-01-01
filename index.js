cfe79daf182b74b3b28e13e13722aa0ece310677
nadia-newaz <nadia.nitu55@gmail.com>
2023-01-01 16:36:02 +0600

    js project

---
+++ index.js
@@ -0,0 +1,26 @@
+// == function with out parameter == 
+// let number = 11;
+
+// function evenOdd(){
+//     if(number % 2 == 0){
+//         console.log("this number is even");
+//     }
+//     else{
+//         console.log("this is a odd number");
+//     }
+
+// }
+// evenOdd();
+
+// == function with parameter == 
+
+function evenOddNumber(number){
+    if(number % 2 == 0){
+        console.log("this is even");
+    }
+    else{
+        console.log("this is odd");
+    }
+
+}
+evenOddNumber(35);
